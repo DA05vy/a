ade7635048d6a5e896ff90f2c1cab56328566118
DA05vy <dda05vy@gmail.com>
2025-11-25 22:17:01 +0700

    Add files via upload

---
+++ NhanVien.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class NhanVien
+    {
+        public int MaNV {  get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string PhongBan { get; set; }
+        public double HeSoLuong { get; set; }
+        public int Tuoi
+        {
+            get { return DateTime.Now.Year - NgaySinh.Year; }
+        }
+
+    }
+}
